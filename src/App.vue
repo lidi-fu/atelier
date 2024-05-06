09ad68bce56b1fb9f9e3c9a46ba20f3b7ef04b3c
Miu-Lee <lidi.fu@icloud.com>
2024-05-06 08:47:03 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link class="nav-element" to="/">Home</router-link>
+    <router-link class="nav-element" to="/works">Works</router-link>
+    <router-link class="nav-element" to="/Archive">Archive</router-link>
+    <router-link class="nav-element" to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
@@ -25,6 +27,10 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #2b85ec;
+}
+
+.nav-element {
+  margin: 10px;
 }
 </style>
---
+++ src/views/WorksView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>This is works page</h2>
+</template>
+
+<script>
+export default {
+    name: WorksView,
+    components: {
+        WorksView
+    }
+}
+</script>
+
+<style>
+
+</style>
